fdee6927c164502732e53941d97212a24415bb94
gank <kkgan406@163.com>
2026-09-24 17:50:03 +0800

    feat: expand mooncake game items

--- src/config/game.ts
+++ src/config/game.ts
@@ -2,5 +2,5 @@ export const game = {
   targetScore: 520,
   gameDuration: 36,
   difficulty: 1,
-  itemScores: { mooncake: 8, golden: 25, osmanthus: 20, starlight: 50, lucky: 80, cloud: -30, fiveKernel: 1 },
+  itemScores: { mooncake: 8, eggYolk: 16, redBean: 12, golden: 25, osmanthus: 20, starlight: 50, lucky: 40, cloud: -30, fiveKernel: 1 },
 }
